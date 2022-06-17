caae20fcdf693008748af0a5560ff4c17eaa9e7e
Zeynalbey <zeynaleg@code.edu.az>
2022-06-17 15:20:48 +0400

    yenilikler

---
+++ Classlar/Classlar/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Valyuta
+{                                                      //valyuta haqqin class//
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Valyuta birincivalyuta = new Valyuta("Kokoin", "mokoin", "tacikistan", 1000)
+            {
+            };
+            Valyuta ikincivalyuta = new Valyuta("kuza", "muza", "basqirdistan", 5000)
+            {
+            };
+            Valyuta ucuncuvalyuta = new Valyuta("durba", "murba", "hindistan", 3000)
+            {
+            };
+            Valyuta dorduncuvalyuta = new Valyuta("mina", "gina", "pakistan", 2000)
+            {
+            };
+
+            //ikincivalyuta.Start();
+            //Console.WriteLine(ikincivalyuta.kursunVeziyyeti);
+            //ikincivalyuta.Start();
+
+            //ikincivalyuta.Stop();
+            //Console.WriteLine(ikincivalyuta.kursunVeziyyeti);
+            //ucuncuvalyuta.Start();
+            //ucuncuvalyuta.Start();
+            //ucuncuvalyuta.Stop();
+            //Console.WriteLine(ucuncuvalyuta.kursunVeziyyeti);
+            //ucuncuvalyuta.Stop();
+            Console.WriteLine(birincivalyuta._olke);
+        }
+    }
+    class Valyuta
+    {
+        public Valyuta(string name, string valyutaindex, string olke, double kurs)
+        {
+            _name = name;
+            _olke = olke;
+            _valyutaindex = valyutaindex;
+            _kurs = kurs;
+        }
+        public Valyuta(string name)
+        {
+            _name = name;
+        }
+        public Valyuta(string name, string olke)
+            : this(name)
+        {
+            _olke = olke;
+        }
+        public bool kursunVeziyyeti;
+        public string _name;
+        public string _valyutaindex;
+        public string _olke;
+        public double _kurs;
+        public void Start()
+        {
+            if (kursunVeziyyeti == true)
+            {
+                Console.WriteLine($"{_name} artiq ise dusub");
+            }
+            else
+            {
+                kursunVeziyyeti = true;
+                Console.WriteLine($"{_name} ise sal");
+            }
+        }
+        public void Stop()
+        {
+            if (kursunVeziyyeti == false)
+            {
+                Console.WriteLine($"{_name} artiq dayanib");
+            }
+            else
+            {
+                kursunVeziyyeti = false;
+                Console.WriteLine($"{_name} saxla");
+            }
+            
+        }
+    }
+}
---
+++ New folder/ConsoleApp36/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human firsthuman = new Human("Fazil", " qarayev", "Gulmemmed", 52, "male", "Azeri", 5);
+            {
+
+
+            }
+            Human secondhuman = new Human("maksuq", " cunayev", "kapasito", 41, "female", "rus", 1);
+            {
+
+            }
+            Human thirdhuman = new Human("mamiko", " arayan", "riko", 69, "male", "ARmyan", 4);
+            {
+
+            }
+            Console.WriteLine(thirdhuman.name);
+
+        }
+
+    }
+    class Human
+    {
+        public Human(string _name, string _lastName, string _dadsName, int _age, string _gender, string _nathion, int _bloodGroup)
+        {
+            name = _name;
+            lastName = _lastName;
+            dadsName = _dadsName;
+            age = _age;
+            gender = _gender;
+            nathion = _nathion;
+            bloodGroup = _bloodGroup;
+        }
+
+        public string name;
+        public string lastName;
+        public string dadsName;
+        public int age;
+        public string gender;
+        public string nathion;
+        public int bloodGroup;
+    }
+}
